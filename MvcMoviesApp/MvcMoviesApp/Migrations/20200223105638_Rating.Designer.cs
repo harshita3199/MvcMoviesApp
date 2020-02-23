ae1d11254e109039a67d549352435d3220241cad
harshita3199 <harshitagupta@jklu.edu.in>
2020-02-23 17:00:18 +0530

    Mvc Movies App

--- MvcMoviesApp/MvcMoviesApp/Migrations/20200223105638_Rating.Designer.cs
+++ MvcMoviesApp/MvcMoviesApp/Migrations/20200223105638_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MvcMoviesApp.Data;
 
 namespace MvcMoviesApp.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200223105638_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
